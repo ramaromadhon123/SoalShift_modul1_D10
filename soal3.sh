38a3a37e833565517775861259c3bfca62aea9b1
Shella Nainggolan <37216864+shellaagn@users.noreply.github.com>
2019-03-02 13:34:31 +0700

    Add files via upload

--- soal3.sh
+++ soal3.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Masukkan jumlah password yang akan dibuat: "
-read pwd
+i=1
+tes=1
 
-for((num=1; num<=n; num+=1))
+while [ $i -ne 0 ]
 do
-	if [ -f ~/Password/password$i.txt ]
-	then pwd=$pwd+1
+	if [[ -f ~/Password/password$tes.txt ]]
+	then
+	
+	tes=$((tes + 1))
 
 	else
-	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > ~/Password/password$i.txt
-
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > ~/Password/password$tes.txt
+	i=0
 	fi
+
 done
